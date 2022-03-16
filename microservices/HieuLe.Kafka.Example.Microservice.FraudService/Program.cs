bca0bcdf3f7e6df474335bfb58210b2a4542d9ac
Hieu Le <letrunghieu.cse09@gmail.com>
2022-03-16 18:00:16 +0700

    Refactor the solution structure

---
+++ microservices/HieuLe.Kafka.Example.Microservice.FraudService/Program.cs
@@ -0,0 +1,10 @@
+using HieuLe.Kafka.Example.Microservice.FraudService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
